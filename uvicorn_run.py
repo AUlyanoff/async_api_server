2d12934396322accdaf2cc9e7087f00d7ce989b9
Ulyanoff <129964604+AUlyanoff@users.noreply.github.com>
2024-03-08 20:12:25 +0300

    Add files via upload

---
+++ uvicorn_run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    from warnings import warn_explicit
+    from datetime import datetime
+    import uvicorn
+
+    warn_explicit(f"\nMDM-server (uvicorn {uvicorn.__version__}) started at "
+                  f"{datetime.now().strftime('%d-%m-%Y %H:%M:%S, %A')}...",
+                  category=UserWarning, filename='', lineno=-1)
+
+    from config import config
+    cfg = config.uvicorn
+    uvicorn.run("app.app_init:app", host=cfg.host, port=cfg.port, log_level=cfg.log, workers=cfg.workers)
