5dc18b9a78d2482efb6e86b4e37b52af3c12eae6
Ulyanoff <129964604+AUlyanoff@users.noreply.github.com>
2024-03-08 21:02:27 +0300

    Add files via upload

---
+++ src/app/ios/view.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from fastapi import Request, APIRouter
+
+from database import db, DBConnection
+
+ios_router = APIRouter()
+
+logger = logging.getLogger(__name__)
+
+
+@ios_router.put("/mdm")
+async def mdm(request: Request, conn: DBConnection):
+    """Тестовый роут"""
+    res, rc = await db.sp_kit_imdm_get_next_command_for_ios(conn, 102)
+    res, = await db.sp_monitor_imdm_get_device_token(conn, 102)
+    res, ws, uids, policy = await db.sp_profile_imdm_get_ios_monitor_profile(conn, 102)
+    return ''
